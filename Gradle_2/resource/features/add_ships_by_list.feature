0455ba7d7a09d7faeff56be467306f9379784d31
vijayrao21 <vijayrao21@gmail.com>
2018-05-21 11:10:56 +0100

    Add files via upload

---
+++ Gradle_2/resource/features/add_ships_by_list.feature
@@ -0,0 +1,5 @@
+Feature: 
+Test project
+
+  Scenario: connection check
+    Given testing cucumber feature
---
+++ Gradle_2/src/test/java/stepDefinition/Test.java
@@ -0,0 +1,18 @@
+package stepDefinition;
+
+import org.junit.Assert;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+
+public class Test{
+
+	@Given("testing cucumber feature")
+	public void testing_cucumber_feature() {
+System.out.println("Testing cucumber print");
+	}
+
+
+}
